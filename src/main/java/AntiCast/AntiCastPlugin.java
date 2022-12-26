9ede422b0dfda70e912b6e77642d0ec0eae61884
Ethan Roche <53758442+Arkenstine@users.noreply.github.com>
2022-12-26 15:34:33 +0530

    Add files via upload

---
+++ src/main/java/AntiCast/AntiCastPlugin.java
@@ -0,0 +1,150 @@
+package AntiCast;
+
+import java.util.LinkedList;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.block.Block;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+/*
+ * AntiCast java plugin
+ * 
+ * @author Sam Belliveau (sam.belliveau@gmail.com)
+ */
+public class AntiCastPlugin extends JavaPlugin implements Listener  {
+    FileConfiguration config = getConfig();
+
+    public void onEnable() {
+        
+        config.addDefault("bounds.x.min", -500);
+        config.addDefault("bounds.x.max", 500);
+        config.addDefault("bounds.y.min", -500);
+        config.addDefault("bounds.y.max", 500);
+
+        config.addDefault("casts.sampletime", 15.0); 
+        config.addDefault("casts.max.warn", 16);
+        config.addDefault("casts.max.stop", 24);       
+
+        config.addDefault("notifs.useAdminChannel", true);
+
+        config.options().copyDefaults(true);
+        saveConfig();
+        
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    /*********************/
+    /*** LISTENER PART ***/
+    /*********************/
+
+    // Booleans used to make sure console doesnt get spammed with messages
+    private boolean cancelled = false;
+    private int lastX = -1, lastY = -1;
+    private LinkedList<Long> casts = new LinkedList<>();
+
+    @EventHandler
+    public void onCobbleFormation(BlockFormEvent e) {
+        // The current time of this call
+        long now = System.currentTimeMillis();
+
+        // Get Config Values
+        final int MIN_X = config.getInt("bounds.x.min");
+        final int MAX_X = config.getInt("bounds.x.max");
+        final int MIN_Y = config.getInt("bounds.y.min");
+        final int MAX_Y = config.getInt("bounds.y.max");
+
+        // Get the information of the Block
+        Block block = e.getBlock();
+        Material type = block.getType();
+        int x = block.getX();
+        int y = block.getY();
+
+        // Check if x and y coords are valid
+        boolean xvalid = MIN_X <= x && x <= MAX_Y;
+        boolean yvalid = MIN_Y <= y && y <= MAX_Y;
+
+        if (xvalid && yvalid && (type == Material.LAVA || type == Material.WATER)) {
+            // Add time of cast to lists
+            casts.addLast(now);
+            lastX = x; 
+            lastY = y;
+
+            refresh();
+
+            if(cancelled) {
+                // Cancel the formation of cobble or any other block
+                e.setCancelled(true);
+                block.setType(Material.AIR);
+            }
+        }
+    }
+
+    // Check things like old casts, and remove an
+    public void refresh() {
+        // The current time of this call
+        long now = System.currentTimeMillis();
+
+        // Get Config Values
+        final int SAMPLE_TIME = (int)(config.getDouble("casts.sampletime") * 1000);
+        final int MAX_WARN = config.getInt("casts.max.warn");
+        final int MAX_STOP = config.getInt("casts.max.stop");
+        final boolean USE_ADMIN = config.getBoolean("notifs.useAdminChannel");
+
+        // Ignore any casts that are too old
+        while(0 < casts.size() && SAMPLE_TIME < (now - casts.getFirst())) {
+            casts.removeFirst();
+        }
+
+        // Get number of casts in sample time
+        int size = casts.size();
+
+        // Check to see how many casts have happened
+        if(size > MAX_STOP) {
+            cancelled = true;
+
+        } else if (size > MAX_WARN) {
+            // Only write warning if the cast hasnt been cancelled
+            if(!cancelled) {
+                long age = (now - casts.getFirst()) / 1000;
+                String msg = String.format("%d casts at spawn in past %d secs [%d, %d]", size, age, lastX, lastY);
+                Broadcast.warning(msg, USE_ADMIN);
+            } 
+        } else {
+            // Uncancel once the value gets low enough
+            if(cancelled) {
+                Broadcast.good("Enabled Lava & Water Interactions At Spawn!", false);
+                cancelled = false;
+            }
+        }
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("sbalc")) {
+            refresh();
+
+            final int CASTS  = casts.size();
+            final double SAMPLE_TIME = config.getDouble("casts.sampletime");
+
+            sender.sendMessage(Broadcast.HEADER + Broadcast.GOOD + "SBALC v1.2:");
+            sender.sendMessage(Broadcast.HEADER + Broadcast.NORMAL + "    - Current Interactions: ");
+
+            if(cancelled) {
+                sender.sendMessage(Broadcast.HEADER + Broadcast.NORMAL + "        - Lava & Water: Disabled");
+            } else {
+                sender.sendMessage(Broadcast.HEADER + Broadcast.NORMAL + "        - Lava & Water: Enabled");
+            }
+
+            sender.sendMessage(Broadcast.HEADER + Broadcast.NORMAL + "        - " + CASTS + " casts over " + SAMPLE_TIME + "s");
+            sender.sendMessage(Broadcast.HEADER + Broadcast.NORMAL + "        - Coords of last cast: [" + lastX + ", " + lastY + "]");
+
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/AntiCast/Broadcast.java
@@ -0,0 +1,45 @@
+package AntiCast;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.ChatColor;
+
+public final class Broadcast {
+
+    public static final String HEADER = "" +
+        ChatColor.RESET + 
+        ChatColor.BOLD + 
+        ChatColor.DARK_AQUA + "[" +
+        ChatColor.AQUA + "SBALC" +
+        ChatColor.DARK_AQUA + "]" +
+        ChatColor.RESET + ChatColor.WHITE + " ";
+
+    public static final String NORMAL   = ChatColor.GRAY.toString();
+    public static final String GOOD     = ChatColor.GREEN.toString();
+    public static final String WARNING  = ChatColor.YELLOW.toString();
+    public static final String CRITICAL = ChatColor.RED.toString();
+    
+    private static void broadcast(String msg, boolean admins) {
+        if(admins) {
+            Bukkit.broadcast(HEADER + msg, Server.BROADCAST_CHANNEL_ADMINISTRATIVE);
+        } else {
+            Bukkit.broadcastMessage(HEADER + msg);
+        }
+    }
+
+    public static void normal(String msg, boolean admins) {
+        broadcast(NORMAL + msg, admins);
+    }
+
+    public static void good(String msg, boolean admins) {
+        broadcast(GOOD + msg, admins);
+    }
+
+    public static void warning(String msg, boolean admins) {
+        broadcast(WARNING + msg, admins);
+    }
+
+    public static void critical(String msg, boolean admins) {
+        broadcast(CRITICAL + msg, admins);
+    }
+}
